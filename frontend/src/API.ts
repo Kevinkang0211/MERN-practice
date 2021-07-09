d500bc8c811f902f28d430a6ef552bf6670eed65
Kevinkang0211 <kevinkang870211@gmail.com>
2021-07-09 10:44:41 +0000

    3 places in backend/1 place in frontend

--- frontend/src/API.ts
+++ frontend/src/API.ts
@@ -1,10 +1,26 @@
 import axios, { AxiosResponse } from 'axios'
 
+/*
 const getTodos = async (): Promise<AxiosResponse<Array<ITodo>>> => {
 // TODO: Should call GET endpoint
+    try {
+        const todos = await axios.get('/api/todos')
+        return todos
+    } catch (error) {
+        console.error(`GET /api/todos ERROR: ${error}`)
+        throw new Error(error)
+    }
 }
 
-// TODO: Should call POST endpoint
+
+const addTodo = async (todoBody: ITodo): Promise<AxiosResponse<Array<ITodo>>> => {
+    // TODO: Should call POST endpoint
+        try {
+        } catch (error) {
+            
+         }
+    }
+
 
 const updateTodo = async (todoBody: ITodo): Promise<AxiosResponse<ITodo>> => {
 // TODO: Should call PUT endpoint
@@ -13,5 +29,54 @@ const updateTodo = async (todoBody: ITodo): Promise<AxiosResponse<ITodo>> => {
 const deleteTodo = async (id: string): Promise<AxiosResponse> => {
 // TODO: Should call DELETE endpoint
 }
+*/
+const getTodos = async (): Promise<AxiosResponse<Array<ITodo>>> => {
+    try {
+        const todos = await axios.get('/api/todos')
+        return todos
+    } catch (error) {
+        console.error(`GET /api/todos ERROR: ${error}`)
+        throw new Error(error)
+    }
+}
+
+const addTodo = async (todoBody: ITodo): Promise<AxiosResponse<ITodo>> => {
+    try {
+        const newTodo = {
+            ...todoBody,
+            status: false
+        }
+        const todo = await axios.post('/api/todos', newTodo)
+        return todo
+    } catch (error) {
+        console.error(`POST /api/todos ERROR: ${error}`)
+        throw new Error(error)
+    }
+}
+
+const updateTodo = async (todoBody: ITodo): Promise<AxiosResponse<ITodo>> => {
+    try {
+        const newTodo = {
+            ...todoBody,
+            status: true
+        }
+        const todo = await axios.put(`/api/todos/${todoBody._id}`, newTodo)
+        return todo
+    } catch (error) {
+        console.error(`PUT /api/todos/${todoBody._id} ERROR: ${error}`)
+        throw new Error(error)
+    }
+}
+
+const deleteTodo = async (id: string): Promise<AxiosResponse> => {
+    try {
+        const res = await axios.delete(`/api/todos/${id}`)
+        return res
+    } catch (error) {
+        console.error(`DELETE /api/todos/${id} ERROR: ${error}`)
+        throw new Error(error)
+    }
+}
+
 
 export { getTodos, addTodo, updateTodo, deleteTodo }
